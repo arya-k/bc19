cb0d03cb8f9de6615a35fec389912e5c387e2e41
AjithK14 <kemisettia@gmail.com>
2019-01-13 13:28:33 -0500

    moved work to sprint folder and put old bot back in ajith folder

--- bots/sprint/attackManager.js
+++ bots/sprint/attackManager.js
@@ -1,5 +1,5 @@
 import {SPECS} from 'battlecode';
-import {CONSTANTS,COMM8,COMM16,CIRCLES} from './constants.js'
+import {CONSTANTS,ATTACK_RANGES_MAX,ATTACK_RANGES_MIN,VISIBLE_RANGES,COMM8,COMM16,CIRCLES} from './constants.js'
 import {move_towards, move_to} from './path.js'
 
 
@@ -17,27 +17,41 @@ import {move_towards, move_to} from './path.js'
 // if you want to make no move, you MUST return null.
 // If you want just the 8 points around a given spot, use CIRCLES[2]
 
+function dist(my_location,other_location){
+  return (my_location[0]-other_location.x)**2 + (my_location.y-other_location.y)**2
+}
 // PSEUDOCODE
 function defensiveBehavior(self, mode_location, base_location) {
   // If you see the enemy, engage (moving towards if you need to).
   // Once you've killed the enemy, return to castle/church and deposit resources.
-  if (can see enemy outside attackrange) {
-    A* towards enemy
-  } else if (can see enemy in attackrange) {
-    attack the enemy
-  } else if (modelocation is not null) {
-    if (mode_location is out of visible range) {
-      A* towards modelocation
+  var visibleRobots = this.getVisibleRobots()
+  var unit = self.unit
+  visibleRobots.forEach(function(r){
+    if (self.team !== r.team){
+      distance = ((r.x-self.x)**2)+((r.y-self.y)**2)
+      if (distance > ATTACK_RANGES_MAX[unit]){
+        return move_towards(this.getPassableMap(),this,getVisibleRobotMap(),[self.x,self.y],[r.x,r.y],ATTACK_RANGES_MIN[unit],ATTACK_RANGES_MAX[unit])
+      }
+      else if (distance <= ATTACK_RANGES_MAX[unit]){
+        return this.attack(r.x-self.x,r.y-self.y)
+      }
+    }
+
+  })
+  if (mode_location !== null) {
+    if ((((mode_location.x-self.x)**2)+((mode_location.y-self.y)**2)) > VISIBLE_RANGES[unit]) {
+        return move_to(this.getPassableMap(),this.getVisibleRobotMap(),[self.x,self.y],[r.x,r.y])
     } else { // we've killed the enemy
-      return CONSTANTS.ELIMINATED_ENEMY;
+        return CONSTANTS.ELIMINATED_ENEMY;
     }
   }
   
   if (mode_location === null) {
-    if (not adjacent to base_location) {
-      A* towards base_location
-    } else if (have resources) {
-      deposit resources
+    if ((((base_location.x-self.x)**2)+((base_location.y-self.y)**2)) > 2) {
+       return move_to(this.getPassableMap(),this.getVisibleRobotMap(),[self.x,self.y],[base_location.x,base_location.y])
+    }
+    else if (self.karbonite > 0) {
+       return(base_location.x-self.x,base_location.y-self.y,self.karbonite,0)
     } else {
       return null; // nothing to do, just camp out.
     }
@@ -48,52 +62,105 @@ function offensiveBehavior(self, mode_location) {
   // Attack enemies in range
   // OR
   // A* toward target
-  visibleRobots = self.visibleRobots()
-  if (enemy in attack_range)
-    return self.attack(enemy)
-  else if (enemy in visibleRobots)
-    return self.move(A* in enemydirection)
-  else if (cant see mode_location) {
-    return self.move(A* toward mode_location)
-  } else {
+  var visibleRobots = this.getVisibleRobots()
+  var unit = self.unit
+  visibleRobots.forEach(function(r){
+    if (self.team !== r.team){
+      distance = ((r.x-self.x)**2)+((r.y-self.y)**2)
+      if (distance > ATTACK_RANGES_MAX[unit]){
+        return move_towards(this.getPassableMap(),this,getVisibleRobotMap(),[self.x,self.y],[r.x,r.y],ATTACK_RANGES_MIN[unit],ATTACK_RANGES_MAX[unit])
+      }
+      else if (distance <= ATTACK_RANGES_MAX[unit]){
+        return this.attack(r.x-self.x,r.y-self.y)
+      }
+    }
+
+  })
+  else if ((((mode_location.x-self.x)**2)+((mode_location.y-self.y)**2)) > VISIBLE_RANGES[unit]) {
+      return move_to(this.getPassableMap(),this.getVisibleRobotMap(),[self.x,self.y],[r.x,r.y])
+  }
+  else {
     return CONSTANTS.ELIMINATED_ENEMY;
   }
   
 function escortBehavior(self, pilgrim_id) {
-  if (church is next to pilgrim_id) {
-    return CONSTANTS.ABANDON_ESCORT
-  }
-  else if (enemies in attack_range) {
-    attack the enemy
-  } else {
-    // you don't need A* for this one, just for loop through all possible moves.
-    return self.move(smallest step that minimizes r^2 between me and my pilgrim)
+  var robotMap = this.getVisibleRobotMap()
+  var pilgrim = this.getRobot(pilgrim_id)
+  var pilgrimX = pilgrim.x
+  var pilgrimY = pilgrim.y
+  CIRCLES[2].forEach(function(r){
+    if (this.getRobot[robotMap[r[1]+pilgrimY][r[0]+pilgrimX]].unit==1){
+      return CONSTANTS.ABANDON_ESCORT
+    }
+  })
+
+  var visibleRobots = this.getVisibleRobots()
+  var unit = self.unit
+  visibleRobots.forEach(function(r){
+    if (self.team !== r.team){
+      if (distance <= ATTACK_RANGES_MAX[unit]){
+        return this.attack(r.x-self.x,r.y-self.y)
+      }
+    }
+
+  })
+  
+  else {
+    var dx = pilgrimX-self.x
+    dx = (dx/Math.abs(dx))*(Math.abs(dx)-1)
+    var dy = pilgirmY-self.y
+    dy = (dy/Math.abs(dy))*(Math.abs(dy)-1)
+    var dr = (dx*dx)+(dy*dy)
+    if (dr <= 9)
+    {
+      return this.move(dx,dy)
+    }
+    else{
+      return this.move((dx/Math.abs(dx))*2,(dy/Math.abs(dy))*2)
+    }
   }
 
 }
 
 function randomMoveBehavior() {
-  // Just move randomly.
-  visibleRobots = self.visibleRobots()
-  if (enemy in attack_range)
-    return this.attack(enemy)
-  else if (enemy in visibleRobots)
-    return this.move(A* in enemydirection)
-  else
-    move in a random direction;
+  var visibleRobots = this.getVisibleRobots()
+  var unit = self.unit
+  visibleRobots.forEach(function(r){
+    if (self.team !== r.team){
+      distance = ((r.x-self.x)**2)+((r.y-self.y)**2)
+      if (distance > ATTACK_RANGES_MAX[unit]){
+        return move_towards(this.getPassableMap(),this,getVisibleRobotMap(),[self.x,self.y],[r.x,r.y],ATTACK_RANGES_MIN[unit],ATTACK_RANGES_MAX[unit])
+      }
+      else if (distance <= ATTACK_RANGES_MAX[unit]){
+        return this.attack(r.x-self.x,r.y-self.y)
+      }
+    }
+
+  })
+  var pass = this.getPassableMap()
+  CIRCLES[1].forEach(function(r){
+    if ((self.y+r[1])>0 && (self.x+r[0])>0 && (self.y+r[1])<pass.length && (self.x+r[0])<pass[0].length && pass[self.y+r[1]][self.x+r[0]]){
+      return this.move(r.x-self.x,r.y-self.y)
+    }
+  })
 }
 
 
 class CrusaderManager() {
-  function init() {
-    this.pass_map = getPassableLocations();
+  function constructor(startx,starty,pass_map,visibleRobots) {
+    this.pass_map = pass_map;
     this.mode = CONSTANTS.DEFENSE
     this.mode_location = [];
-    this.base_location == the church OR castle immediately next to you;
+    for (let r of CIRCLES[2]) {
+      if (visibleRobots[starty+r[1]][startx+r[0]].unit == 0 || visibleRobots[starty+r[1]][startx+r[0]].unit == 1) {
+        this.base_location={x:startx+r[0],y:starty+r[1],}
+        break;
+      }
+    }
   }
   function turn(step, self) {
 
-    if (signal & COMM16.HEADER_MASK == COMM16.ESCORT_HEADER) { // this is how you figure out the signal type.
+    if (signal && COMM16.HEADER_MASK == COMM16.ESCORT_HEADER) { // this is how you figure out the signal type.
       this.mode = CONSTANTS.ESCORT
       this.mode_location = null;
       this.base_location = COMM16.DECODE_ESCORT(escort_signal) // base_location is the pilgrim id
