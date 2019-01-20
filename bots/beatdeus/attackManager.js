58947f4b64ecc0620dfcc798369a32f4dbbb47db
AjithK14 <kemisettia@gmail.com>
2019-01-20 14:38:51 -0500

    back and forth debugged?

--- bots/beatdeus/attackManager.js
+++ bots/beatdeus/attackManager.js
@@ -2,7 +2,7 @@ import {SPECS} from 'battlecode';
 import {CONSTANTS, CIRCLES} from './constants.js'
 import {move_towards, move_to, no_swarm} from './path.js'
 import {COMM8,COMM16} from './comm.js'
-import {getAttackOrder, has_adjacent_castle, getNearbyRobots, dist, is_valid} from './utils.js'
+import {getAttackOrder, has_adjacent_castle, getNearbyRobots, dist, is_valid, is_passable} from './utils.js'
 
 function Point(x, y, p) {
   this.x = x,
@@ -20,7 +20,6 @@ function nonNuisanceBehavior(self, base_loc) {
   let visited = new Set()
   let queue = [new Point(self.me.x, self.me.y, null)];
   let path_end_point = null;
-  let vis_map = self.getVisibleRobotMap()
 
   let nono_map = [...Array(self.map.length)].map(e => Array(self.map.length).fill(false));
   for (const r of self.getVisibleRobots()) {
@@ -28,24 +27,24 @@ function nonNuisanceBehavior(self, base_loc) {
       if (r.unit == SPECS.CHURCH || r.unit == SPECS.CASTLE) { // castle or church
         nono_map[r.y][r.x] = true;
         for (const dir of CIRCLES[2])
-          if (is_valid(r.x + dir[0], r.y + dir[1], self.map.length) && vis_map[r.y+dir[1]][r.x+dir[0]] <= 0 && self.map[r.y + dir[1]][r.x + dir[0]])
+          if (is_valid(r.x + dir[0], r.y + dir[1], self.map.length))
             nono_map[r.y + dir[1]][r.x + dir[0]] = true;
       } else if (r.id !== self.me.id) {
         nono_map[r.y][r.x] = true;
         for (const dir of CIRCLES[1])
-          if (is_valid(r.x + dir[0], r.y + dir[1], self.map.length) && vis_map[r.y+dir[1]][r.x+dir[0]] <= 0 && self.map[r.y + dir[1]][r.x + dir[0]])
+          if (is_valid(r.x + dir[0], r.y + dir[1], self.map.length))
             nono_map[r.y + dir[1]][r.x + dir[0]] = true;
       }
     }
   }
-
+  
   while (queue.length > 0) {
     current = queue.shift();
 
     if (visited.has((current.y<<6)|current.x)){ continue; }
 
     if (!nono_map[current.y][current.x] && !self.fuel_map[current.y][current.x] &&
-        !self.karbonite_map[current.y][current.x] && self.map[current.y][current.x] && vis_map[current.y][current.x] <= 0) {
+        !self.karbonite_map[current.y][current.x] && self.map[current.y][current.x] && is_passable(self,current.x,current.y)) {
       path_end_point = current;
       break;
     }
@@ -53,7 +52,7 @@ function nonNuisanceBehavior(self, base_loc) {
     visited.add((current.y<<6)|current.x);
     for (const dir of CIRCLES[SPECS.UNITS[self.me.unit].SPEED]) {
       if (is_valid(current.x+dir[0], current.y+dir[1], self.map.length)) {
-        if (vis_map[current.y + dir[1]][current.x + dir[0]] <= 0 && self.map[current.y + dir[1]][current.x + dir[0]]) {
+        if (is_passable(self,current.x,current.y) && self.map[current.y + dir[1]][current.x + dir[0]]) {
           queue.push(new Point(current.x + dir[0], current.y + dir[1], current));
         }
       }
@@ -61,8 +60,9 @@ function nonNuisanceBehavior(self, base_loc) {
   }
 
   if (path_end_point === null || path_end_point.p === null) { // you already good. Move towards the base if you're too far.
+    self.log('we good')
     if (dist([self.me.x, self.me.y], base_loc) >= 25) {
-      self.log("move towards")
+      // self.log("move towards")
       let move = move_towards(self, [self.me.x, self.me.y], base_loc) // head towards base
       if (move !== null) {
         return [move.x - self.me.x, move.y - self.me.y];
@@ -74,6 +74,8 @@ function nonNuisanceBehavior(self, base_loc) {
     while (path_end_point.p.p !== null){
       path_end_point = path_end_point.p;
     }
+    self.log(path_end_point.x)
+    self.log(path_end_point.y)
     return [path_end_point.x - self.me.x, path_end_point.y - self.me.y];
   }
 }
@@ -235,9 +237,34 @@ function defensive_behaviour_passive(self, mode_location, base_location) {
   // self.log("here1")
 
   //attack if possible
+  let vis_map = self.getVisibleRobotMap()
+  //attack enemy, but MAKE SURE crusader is between prophet and enemy
   let targets = getAttackOrder(self)
-  //self.log('here1')
-  if (targets.length>0){
+  if (targets.length != 0){
+    let crusaders = []
+    let enemies = []
+    //if there is a single enemy robot without a crusader in front, move away
+    //since this method is called by prophets, it must be certain that they are protected by crusaders
+    for (const p of self.getVisibleRobots()){
+      if (p.unit == SPECS.UNITS[SPECS.CRUSADER] || p.unit == SPECS.UNITS[SPECS.PREACHER]){
+        crusaders.push([p.x,p.y])
+      }
+      if (p.team != self.me.team){
+        enemies.push(p)
+      }
+    }
+    for (const r of targets){
+      for (const c of crusaders){
+        if (dist(c,[r.x,r.y])>dist([r.x,r.y,self.me.x,self.me.y])){
+          let move = move_away(self,enemies)
+          if (move !== null) {
+            return self.move(move.x - self.me.x, move.y - self.me.y);
+          } else {
+            return null;
+          }
+        }
+      }
+    }
     return self.attack(targets[0].x-self.me.x,targets[0].y-self.me.y)
   }
 
@@ -260,6 +287,8 @@ function defensive_behaviour_passive(self, mode_location, base_location) {
   //Don't be annoying; get off resources spots and waffle
   else {
     self.log('no nuis');
+    self.log(self.me.x)
+    self.log(self.me.y)
     let n = nonNuisanceBehavior(self,base_location);
     if (n !== null){
       return self.move(n[0],n[1]);
